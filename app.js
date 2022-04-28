ecbc975f5662440d04139c1f40af7b12ff83b23d
kathrynzaro <zarokathryn@gmail.com>
2022-04-28 16:20:06 -0700

    added images, grabbed elems, added event listeners

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 // import functions and grab DOM elements
 const exteriorDropdown = document.getElementById('exterior-dropdown');
+const interiorDropdown = document.getElementById('interior-dropdown');
+const neighborhoodDropdown = document.getElementById('neighborhood-dropdown');
 
 const exElem = document.getElementById('exterior');
+const inElem = document.getElementById('interior');
+const hoodElem = document.getElementById('neighborhood');
 
 // let state
 let exCount = 0;
@@ -16,8 +20,21 @@ exteriorDropdown.addEventListener('change', () => {
     console.log('changing exterior', exteriorDropdown.value);
     exCount++;
     exElem.style.backgroundImage = `url('../assets/${exteriorDropdown.value}.png')`;
-
 });
+
+interiorDropdown.addEventListener('change', () => {
+    console.log('changing interior', interiorDropdown.value);
+    inCount++;
+    inElem.style.backgroundImage = `url('../assets/${interiorDropdown.value}.png')`;
+});
+
+neighborhoodDropdown.addEventListener('change', () => {
+    console.log('changing neighborhood', neighborhoodDropdown.value);
+    hoodCount++;
+    hoodElem.style.backgroundImage = `url('../assets/${neighborhoodDropdown.value}.png')`;
+});
+
+
 
   // get user input
   // use user input to update state 
