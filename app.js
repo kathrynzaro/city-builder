97643e7c3fc84466acf1d8f1a1b748796994a251
kathrynzaro <zarokathryn@gmail.com>
2022-04-28 14:39:27 -0700

    oops forgot to commit, lots of html, some css and some js

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 // import functions and grab DOM elements
+const exteriorDropdown = document.getElementById('exterior-dropdown');
+
+const exElem = document.getElementById('exterior');
 
 // let state
+let exCount = 0;
+let inCount = 0;
+let hoodCount = 0;
+let catCount = 0;
+
+let slogan = [];
 
 // set event listeners 
+exteriorDropdown.addEventListener('change', () => {
+    console.log('changing exterior', exteriorDropdown.value);
+    exCount++;
+    exElem.style.backgroundImage = `url('../assets/${exteriorDropdown.value}.png')`;
+
+});
+
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
